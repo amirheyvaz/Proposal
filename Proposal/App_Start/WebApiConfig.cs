210d34c64d7e751cbefcf6e55644c26265ef4d2f
amirheyvaz <amirheyvaz@gmail.com>
2020-05-02 18:00:09 +0430

    init

--- Proposal/App_Start/WebApiConfig.cs
+++ Proposal/App_Start/WebApiConfig.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Web.Http;
 
 namespace Proposal
@@ -10,6 +11,11 @@ namespace Proposal
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            config.EnableCors();
+
+            // Web API configuration and services
+            config.Filters.Add(new AuthorizeAttribute());
+
 
             // Web API routes
             config.MapHttpAttributeRoutes();
@@ -19,6 +25,9 @@ namespace Proposal
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+
         }
     }
 }
---
+++ Proposal/Controllers/AuthController.cs
@@ -0,0 +1,51 @@
+﻿using Proposal.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+
+namespace Proposal.Controllers
+{
+    [RoutePrefix("api/Authentication")]
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class AuthController : ApiController
+    {
+        [Route("GetToken/{username}/{password}")]
+        [HttpPost]
+        [AllowAnonymous]
+        public string GetToken(string username, string password)
+        {
+            if (CheckUser(username, password))
+            {
+                return JwtManager.GenerateToken(username);
+            }
+            else
+            {
+                throw new HttpResponseException(HttpStatusCode.Unauthorized);
+            }
+
+        }
+
+        private bool CheckUser(string username, string password)
+        {
+            // should check in the database
+            //var UserRep = IocConfig.Container.GetInstance<IUserRepository>();
+
+            //if (UserRep.SelectBy(x => x.UserName == username && x.Password == password).Any())
+            //{
+            //    return true;
+            //}
+            //else
+            //{
+            //    return false;
+            //}
+
+            return true;
+
+        }
+    }
+}
