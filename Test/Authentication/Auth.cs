241826e90f0ed3912cd174b28a88374aa21cb3fd
amirheyvaz <amirheyvaz@gmail.com>
2020-07-17 12:11:56 +0430

    init-test

---
+++ Test/Authentication/Auth.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Test.Authentication
+{
+    [TestFixture]
+    public class Auth
+    {
+        [Test]
+        public void Test_User_Pass()
+        {
+            Assert.That(1 == 1);
+        }
+    }
+}
